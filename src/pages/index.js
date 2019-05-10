5255b521cf76ec719cf5ca3d6465450f5a044cac
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-11 02:31:39 +0530

    Update my info

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World</h1>
+    <p>My name is Mohammad Aziz</p>
+    <p>
+      I am a JavaScript developer. Currently I'm working as a software developer
+      at <a href="https://livehealth.in">LiveHealth</a>
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>

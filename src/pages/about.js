78fe40369b6b9e642de1b693915f7d232e971439
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-12 01:42:21 +0530

    Add first about us page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function About() {
+  return (
+    <Layout>
+      <h1>Mohammad Aziz</h1>
+      <p>
+        Software Developer, <a href="https://livehealth.in">LiveHealth</a>
+      </p>
+    </Layout>
+  )
+}
+
+export default About

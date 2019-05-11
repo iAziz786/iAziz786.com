9395a0225921ba532cac0e605788a02a4870910c
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-11 09:13:57 +0530

    Add blog.js

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Blog() {
+  return (
+    <div>
+      <h1>Blogging!!!</h1>
+      <p>This is only the landing page of my blogging site.</p>
+    </div>
+  )
+}
+
+export default Blog

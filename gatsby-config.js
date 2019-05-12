3b5a534b06e0a1f8d201d2016bc8614409860b62
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-12 22:51:18 +0530

    Update author username

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Mohammad Aziz`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@iaziz786`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

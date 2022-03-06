ba3609cb4e223ccb3092d97b072ac8c7e03e29eb
Mohammad Aziz <aziz@zoop.one>
2022-03-07 00:50:05 +0530

    blog(new): sustaining your personal projects

--- src/fragments.js
+++ src/fragments.js
@@ -1,25 +1,37 @@
 import { graphql } from "gatsby"
 
-export const bannerImage = graphql`
-  fragment bannerImage260 on File {
-    childImageSharp {
-      fluid(maxWidth: 260, traceSVG: { color: "#ED8936" }, quality: 50) {
-        ...GatsbyImageSharpFluid_withWebp_tracedSVG
-      }
-    }
+export const bannerImage = graphql`fragment bannerImage260 on File {
+  childImageSharp {
+    gatsbyImageData(
+      width: 260
+      tracedSVGOptions: {color: "#ED8936"}
+      quality: 50
+      placeholder: TRACED_SVG
+      layout: CONSTRAINED
+    )
   }
-  fragment bannerImage640 on File {
-    childImageSharp {
-      fluid(maxWidth: 640, traceSVG: { color: "#ED8936" }) {
-        ...GatsbyImageSharpFluid_withWebp_tracedSVG
-      }
-    }
+}
+
+fragment bannerImage640 on File {
+  childImageSharp {
+    gatsbyImageData(
+      width: 640
+      tracedSVGOptions: {color: "#ED8936"}
+      placeholder: TRACED_SVG
+      layout: CONSTRAINED
+    )
   }
-  fragment bannerImage720 on File {
-    childImageSharp {
-      fluid(maxWidth: 720, traceSVG: { color: "#ED8936" }, quality: 75) {
-        ...GatsbyImageSharpFluid_withWebp_tracedSVG
-      }
-    }
+}
+
+fragment bannerImage720 on File {
+  childImageSharp {
+    gatsbyImageData(
+      width: 720
+      tracedSVGOptions: {color: "#ED8936"}
+      quality: 75
+      placeholder: TRACED_SVG
+      layout: CONSTRAINED
+    )
   }
+}
 `
